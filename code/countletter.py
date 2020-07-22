766d7c4735899ee666900c1f2714424c43c6405f
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2020-07-22 11:56:31 -0700

    Create countletter.py

---
+++ code/countletter.py
@@ -0,0 +1,16 @@
+# counts the number of occurences of letters in a file
+def count_letters(text):
+    # make empty dictionary to hold results
+    results = {}
+
+    # iterate over text letters
+    for letter in text:
+        if letter not in results:
+            results[letter] = 0
+
+        results[letter] += 1
+
+    return results
+
+
+print(count_letters("google"))
