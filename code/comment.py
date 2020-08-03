b69200f60fc8f3c279bf67da4b73b50f335e7c16
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2020-08-03 11:15:04 -0700

    Create comment.py

---
+++ code/comment.py
@@ -0,0 +1,15 @@
+# The create_python_script function creates a new python script in the current working directory,
+# adds the line of comments to it declared by the 'comments' variable, and returns the size of the new file.
+import os
+
+
+def create_python_script(filename):
+    comments = "# Start of a new Python program"
+    with open(filename, "w") as file:
+        file.write(comments)
+
+    filesize = os.path.getsize(filename)
+    return filesize
+
+
+print(create_python_script("program.py"))
