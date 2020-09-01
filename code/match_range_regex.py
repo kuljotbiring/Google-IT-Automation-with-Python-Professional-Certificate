5856fcdeb8f83864420dccc87a770460a477d2ce
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2020-08-31 19:32:41 -0700

    Create match_range_regex.py

---
+++ code/match_range_regex.py
@@ -0,0 +1,15 @@
+import re
+
+
+def check_punctuation (text):
+    """check if the text passed contains punctuation symbols: commas, periods, colons, semicolons,
+    question marks, and exclamation points."""
+    result = re.search(r"[,.:;?!]", text)
+    return result != None
+
+
+print(check_punctuation("This is a sentence that ends with a period.")) # True
+print(check_punctuation("This is a sentence fragment without a period")) # False
+print(check_punctuation("Aren't regular expressions awesome?")) # True
+print(check_punctuation("Wow! We're really picking up some steam now!")) # True
+print(check_punctuation("End of the line")) # False
